0d8481714d48c3a75b72c06e03b0b9bb1edad142
Chengyu HAN <cyhan@mail.buct.edu.cn>
2022-08-28 16:52:30 +0800

    更正 lua table 赋值

--- src/main.cpp
+++ src/main.cpp
@@ -277,7 +277,7 @@ void luajit_ffi_call(int nlhs, bxArray *plhs[], int nrhs, const bxArray *prhs[])
     // std::string lib_path = bxGetStringDataPr(prhs[0]);
     // -- 为 lua 脚本注入全局变量
     lua["_bex"] = lua.create_table_with("dll_root_path", _plugin_dll_path.generic_string());
-    lua["_bex"]['lua_root_path'] = _plugin_lua_path.generic_string();
+    lua["_bex"]["lua_root_path"] = _plugin_lua_path.generic_string();
 
     /** ---- 主体函数计算 ---- */
     fs::path lua_file = _plugin_lua_path / fs::path("lua_ffi.lua");
